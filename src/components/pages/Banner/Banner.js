53fad35d8b651139c9fd5b6d73282e6756413482
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-05-12 15:31:09 +0600

    add

--- src/components/pages/Banner/Banner.js
+++ src/components/pages/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import banner from '../../../images/banner-1.jpg';
+import banner from '../../../images/banner-3.jpg';
 import './Banner.css';
 
 const Banner = () => {
@@ -7,6 +7,21 @@ const Banner = () => {
         <div className="banner">
             <div className="banner-section">
                 <img src={banner} alt="..." />
+            </div>
+
+            <div className="banner-text">
+               <h1>Find Your Perfect Trip</h1>
+            </div>
+               <div  className="d-flex text-inner mt-5">
+                    <div>
+                       <h3>TOP HOTELS</h3>
+                       <p>Access The Industry’s Leading Tour Site</p>
+                    </div>
+                    <div className='mx-5'>
+                       <h3>LIST OF TOURS</h3>
+                       <p>Access The Industry’s Leading Tour Site</p>
+                    </div>
+               
             </div>
         </div>
     );
