c68d707c4973bac727a72101cf7a7086ea9794fb
Jannat Borna <81918298+JannatBorna@users.noreply.github.com>
2022-05-12 18:30:22 +0600

    add

--- src/components/pages/MiddleBanner/MiddleBanner.js
+++ src/components/pages/MiddleBanner/MiddleBanner.js
@@ -15,7 +15,24 @@ const MiddleBanner = () => {
                     <h2>Learn More. Book More. Earn More.</h2>
                     <p>Visit our Resource center for the latest articles videos to take your business to the next level.</p>
                     <hr className="my-4"/> 
-               </div>   
+               </div>  
+
+               <div className="middle-div">
+                   <div className="text-start">
+                       <h5>PROD<span className="tag-h5">UCT INFORMATION</span></h5>
+                       <p>Need more details on skip-the-line tickets? <br /> Looking for a list of top activities in Rome? <br /> Want to see a video of a tour before you <br /> book? It's all here.</p>
+                   </div>
+
+                   <div className="text-start">
+                       <h5>TRA<span className="tag-h5">INING RESOURCES</span></h5>
+                       <p>Get helpful tips to increase sales and improve <br /> your business using Viator's Travel Agent <br /> Program.</p>
+                   </div>
+
+                   <div className="text-start">
+                       <h5>TRA<span className="tag-h5">VEL GUIDES</span></h5>
+                       <p>Keep your destination knowledge fresh with<br /> insider tips and blog posts about cities and <br /> regions around the world.</p>
+                   </div>
+               </div> 
             </Container>
         </section>
     );
