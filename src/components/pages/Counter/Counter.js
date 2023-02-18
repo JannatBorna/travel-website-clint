ba3d5d2856ee449c8e09cd1bcb8f22ac207a2bc9
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-02-18 19:20:44 +0600

    add

---
+++ src/components/pages/Counter/Counter.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+const Counter = ({className, ...rest}) => {
+   
+   
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Counter;
