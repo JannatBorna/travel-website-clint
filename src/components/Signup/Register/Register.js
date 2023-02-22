a1b53ef68d3dd77e054b1b40d514588d41329051
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-02-22 22:30:09 +0600

    add

--- src/components/Signup/Register/Register.js
+++ src/components/Signup/Register/Register.js
@@ -9,13 +9,13 @@ import './Register.css';
 const Register = () => {
     return (
         <div>
-            <div className='login-bg'>.</div>
-            <div className='login-bg-2'>.</div>
+            <div className='reg-bg'>.</div>
+            <div className='reg-bg-2'>.</div>
 
         <Container>
             <Row>
                 <Col lg={12} md={12} sm={12}>
-                    <div className='login'>
+                    <div className='register'>
                         <LoginNav />
                         <h1 className='mt-3 welcome'>WELCOME</h1>
                         <h2 className='text-white'> <FontAwesomeIcon icon={faBarsStaggered} 
@@ -23,13 +23,13 @@ const Register = () => {
                         <p className='text-white'>Register into Your account</p>
 
                         <form>
-                            <label for="name" className='p-3 mx-4 text-white fw-bold'>Nmae: </label>
-                            <input type="email" id="email" name="email"/>
+                            <label for="name" className='p-3 mx-4 text-white fw-bold'>Name: </label>
+                            <input type="email" id="email" name="email" className='bg-black'/>
                             <br />
                             <label for="femail" className='p-3 mx-4 text-white fw-bold'>Email :</label>
                             <input type="email" id="email" name="email"/>
                             <br />
-                             <label for="psw" className='mx-4 text-white fw-bold'>Password :</label>
+                             <label for="psw" className='p-2 mx-4 text-white fw-bold password'>Password :</label>
                              <input type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*
                             [a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and 
                             one uppercase and lowercase letter, and at least 8 or more 
@@ -43,11 +43,11 @@ const Register = () => {
                         </form>  
                             
                             <div>
-                                <p className='text-white'>New user? <span 
-                                className='text-danger'>SIGN UP</span> now</p>
+                                <p className='text-white'>All ready Register? <span 
+                                className='text-danger'>SIGN IN</span> now</p>
                             </div>
 
-                            <div className='mt-5 sign-up'>
+                            <div className='mt-1 sign-up'>
                                 <a href="/" className='p-2 facebook'><FontAwesomeIcon icon={faFacebookSquare}/> Facebook</a>
                                 <a href="/" className='p-2 mx-2 google'><FontAwesomeIcon icon={faGoogle}/> Google</a>
                                 {/* <a href="/"><FontAwesomeIcon icon={faGithub}/> GitHub</a> */}
