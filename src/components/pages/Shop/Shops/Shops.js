36823233caab97cc0e7388f688344e692ac8c28d
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-02-25 16:33:21 +0600

    add

--- src/components/pages/Shop/Shops/Shops.js
+++ src/components/pages/Shop/Shops/Shops.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Shop from '../Shop/Shop';
 
 const Shops = () => {
+    const [ shops, setShops ] = useState([]);
+
+    useEffect( () =>{
+        fetch('/shop.json')
+        .then(res => res.json())
+        .then(data => setShops(data))
+
+    }, [])
     return (
         <div>
-            <h2>Shops</h2>
+            <h2>Shops:{shops.length}</h2>
+            {
+                shops.map(shop => <Shop
+                key={shop.id}
+                shop={shop}
+                ></Shop>)
+            }
         </div>
     );
 };
