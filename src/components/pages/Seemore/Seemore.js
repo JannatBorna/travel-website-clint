fa29df854fe3efb24b10f21348c2be90c73e76d6
Dilruba Jannat Borna <bornadilruba38@gmail.com>
2023-03-24 01:54:19 +0600

    add

--- src/components/pages/Seemore/Seemore.js
+++ src/components/pages/Seemore/Seemore.js
@@ -13,6 +13,7 @@ const Seemore = () => {
                     <Col lg={6} md={6} sm={12}>
                         <div className='seemore-img'>
                             <img src={seemore} alt="" />
+                            
                             <h3 className='mt-5 text-start'>Tra<span className='text-white theme-color'>vel Text Services</span></h3>
                              <p className='text-start'>Travel texts are simply descriptions of destinations that appear in travel-related media such as magazines, websites, or brochures. They provide information about a particular destination, including its features and highlights. In contrast, destination texts are written by tourist organizations for their own website or publication. These texts typically include more detailed information about a destination than what’s found in travel texts.</p>
                              <p className='text-start'>There are many things that make a good travel text. But, most importantly, the author must remember to entertain the reader, not recruit them. After all, no one wants to read a dry list of facts about a destination. Instead, they want to be entertained and informed about what is available at their chosen destination.</p>
@@ -31,7 +32,6 @@ const Seemore = () => {
                             
                             <h3>Travel <span className='text-white theme-color'>Texts and Descriptions of Destinations</span></h3>
                             <p>There are many things that make a good travel text. But, most importantly, the author must remember to entertain the reader, not recruit them. After all, no one wants to read a dry list of facts about a destination. Instead, they want to be entertained and informed about what is available at their chosen destination.</p>
-                            
                             <p>At clickworker, we know this better than anyone. That’s why we offer information about destinations and travel tips in an engaging way that will have you itching to pack your bags and go! Our tone is casual and our style light-hearted, so you can feel like you’re chatting with a friend who just happens to know everything about the best places to go on vacation.</p>
                             <p>Once you know what type of traveler your target audience is, it’ll be easier to choose the right photos and design. What is the vacation they’re looking to have? If you’re targeting people who want to relax on their vacation, go for light blues and greens. On the other hand, if your target customer is adventurous, then use bright colors and daring fonts. Whatever route you decide to take, make sure the photos complement each other so that there’s a unified feeling across the entire brochure. And don’t forget – color scheme should always match the feel of your destination!</p>
                             <h3>Use <span className='text-white theme-color'>Cases for Travel Texts</span></h3>
